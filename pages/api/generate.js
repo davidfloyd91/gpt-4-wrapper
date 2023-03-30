f5cd883ae0d1389475add2de16a80fe17b73417f
davidfloyd91 <davidfloyd91@gmail.com>
2023-03-29 22:31:41 -0400

    getting started

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -9,30 +9,29 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
-
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal",
-      }
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const { temperature, content } = req.body;
+    const completion = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages: [{ role: "user", content }],
+      temperature,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
+    res
+      .status(200)
+      .json({
+        result: completion.data.choices.reduce(
+          (prev, { message }) => `${prev}${message.content}`,
+          ""
+        ),
+      });
+  } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
@@ -41,22 +40,9 @@ export default async function (req, res) {
       console.error(`Error with OpenAI API request: ${error.message}`);
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   }
-}
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,189 @@
-import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import { useCallback, useState } from "react";
+import styled from "styled-components";
 
-export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+const COLORS = {
+  BLACK: "#0F1321",
+  DARK_BLUE: "#202942",
+  DARK_GRAY: "#262F3C",
+  LIGHT_BLUE: "#879AC4",
+  LIGHT_GRAY: "#627197",
+  PINK: "#9187C4",
+  WHITE: "#EFF1ED",
+};
 
-  async function onSubmit(event) {
-    event.preventDefault();
+const Text = styled.div`
+  color: ${COLORS.LIGHT_BLUE};
+  font-size: 14px;
+  line-height: 132%;
+  margin-bottom: 12px;
+  max-width: 836px;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  border: 1px solid ${COLORS.DARK_GRAY};
+  border-radius: 4px;
+  color: ${COLORS.WHITE};
+  cursor: pointer;
+  font-size: 14px;
+  margin-bottom: 24px;
+  padding: 4px 12px;
+
+  &:hover {
+    background-color: ${COLORS.DARK_BLUE};
+    border: 1px solid ${COLORS.LIGHT_GRAY};
+  }
+
+  &:disabled {
+    background-color: transparent;
+    border: 1px solid ${COLORS.DARK_GRAY};
+    color: ${COLORS.LIGHT_GRAY};
+    cursor: auto;
+  }
+`;
+
+const Header = styled.h1`
+  color: ${COLORS.PINK};
+`;
+
+const Loading = styled(Text)``;
+
+const OldAnswer = styled(Text)``;
+
+const OldQABlock = styled.div`
+  margin-bottom: 24px;
+`;
+
+const OldQuestion = styled(Text)`
+  color: ${COLORS.WHITE};
+`;
+
+const Question = styled.div`
+  & textarea {
+    background-color: ${COLORS.DARK_GRAY};
+    border: 2px solid transparent;
+    border-radius: 4px;
+    color: ${COLORS.WHITE};
+    margin-bottom: 8px;
+    outline: none;
+    padding: 8px;
+    resize: none;
+
+    &:focus {
+      border: 2px solid ${COLORS.LIGHT_GRAY};
+    }
+  }
+`;
+
+const StyledError = styled.div`
+  color: ${COLORS.PINK};
+`;
+
+const Home = () => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [oldAnswers, setOldAnswers] = useState([]);
+  const [oldQuestions, setOldQuestions] = useState([]);
+  const [question, setQuestion] = useState("");
+  const [temperature, setTemperature] = useState(0.6);
+
+  const handleChange = useCallback(
+    (e) => {
+      setQuestion(e.target.value);
+    },
+    [setQuestion]
+  );
+
+  const handleSubmit = useCallback(async () => {
+    setError("");
+    setLoading(true);
+
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ content: question, temperature }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
       }
 
-      setResult(data.result);
-      setAnimalInput("");
-    } catch(error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
+      setQuestion("");
+      setOldAnswers([...oldAnswers, data.result]);
+      setOldQuestions([...oldQuestions, question]);
+    } catch (_error) {
+      console.error(_error);
+      setError(_error);
+    } finally {
+      setLoading(false);
     }
-  }
+  }, [
+    question,
+    oldAnswers,
+    oldQuestions,
+    setError,
+    setLoading,
+    setOldAnswers,
+    setOldQuestions,
+  ]);
 
   return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
-      </Head>
-
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+    <>
+      <style jsx global>{`
+        body {
+          background-color: ${COLORS.BLACK};
+          color: ${COLORS.WHITE};
+          font-family: monospace;
+          padding: 0 12px;
+        }
+      `}</style>
+      <div>
+        <Header>GPT-4 API Wrapper</Header>
+        {oldQuestions.map((oldQuestion, index) => {
+          const oldAnswer = oldAnswers[index];
+          return (
+            <OldQABlock key={`${index}-${oldQuestion.slice(0, 25)}`}>
+              <OldQuestion>{oldQuestion}</OldQuestion>
+              <OldAnswer>{oldAnswer}</OldAnswer>
+            </OldQABlock>
+          );
+        })}
+        {error && <StyledError>{error}</StyledError>}
+        <Question>
+          <textarea
+            type="textarea"
+            cols={100}
+            name="wut"
+            placeholder="hi"
+            rows={12}
+            value={question}
+            spellcheck="false"
+            onChange={handleChange}
           />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
-      </main>
-    </div>
+        </Question>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "836px",
+          }}
+        >
+          <Button onClick={handleSubmit} disabled={!question || loading}>
+            Submit
+          </Button>
+          {loading && <Loading>Just a second ...</Loading>}
+        </div>
+      </div>
+    </>
   );
-}
+};
+
+export default Home;
